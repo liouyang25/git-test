0633d5ec664889c7ddfef5289e2ebebb62740c9e
liouyang25 <liouyang25@gmail.com>
2023-11-26 15:01:03 +0800

    first commit

---
+++ src/com/liouyang25/GitTest.java
@@ -0,0 +1,13 @@
+package com.liouyang25;
+
+/**
+ * @author: 李卫
+ * @email: liouyang25@gmail.com
+ * @create: 2023-11-26 14:45
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
